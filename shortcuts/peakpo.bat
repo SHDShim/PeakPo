bef4acd525bd3a0a8060325198d6cbdae82bc83d
S.-H. Dan Shim <SHDShim@gmail.com>
2026-01-15 23:07:02 -0700

    update shortcuts for 7.9.x

--- shortcuts/peakpo.bat
+++ shortcuts/peakpo.bat
@@ -2,10 +2,10 @@ rem Step 1: Activate Miniconda or anaconda, use the actual path where your Minic
 call "C:\Users\yourname\miniconda3\Scripts\activate.bat" "C:\Users\yourname\miniconda3"
 
 rem Step 2: Activate Conda environment. 
-call conda activate pkpo2022fbs
+call conda activate pkpo2026
 
 rem Step 3: Change directory to the desired folder.
-cd /d "C:\Users\Shim\peakpo7.8.0\peakpo"
+cd /d "C:\Users\Shim\peakpo7.9.0\peakpo"
 
 rem Step 4: Run the Python script.
 python -m peakpo
--- shortcuts/peakpo.command
+++ shortcuts/peakpo.command
@@ -2,7 +2,7 @@
 # you can move this script to /Applications folder
 # make sure: chmod +x peakpo.command
 eval "$(conda shell.bash hook)"
-conda activate pkpo2022fbs # replace the environment name
+conda activate pkpo2026 # replace the environment name
 cd ~/your/peakpo/folder # replace the path for PeakPo installation
 python -m peakpo
 # if your mac is Apple silicon, then make sure to run with Rosetta.
