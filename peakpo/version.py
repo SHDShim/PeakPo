2597ace8de91d97ea3f9b4efa4da822ed2e21bb0
S.-H. Dan Shim <SHDShim@gmail.com>
2026-07-08 15:12:06 -0700

    7.12.1b another attempt to fix exit problem in windows.

--- peakpo/version.py
+++ peakpo/version.py
@@ -4,9 +4,9 @@ PeakPo version
 Todo:
 - azimuthal integration improvement
 """
-__version__ = "7.12.1a"
+__version__ = "7.12.1b"
 """
-7.12.1a: Second attempt to fix windows exit problem
+7.12.1b: Third attempt to fix windows exit problem
 7.12.0: update peak fitting and unit cell refining instructions.
 7.11.13: overhaul Peak and Cell Fit workflows with improved provenance, 
           interactive peak adjustment (drag-and-drop), and automated result 
